73285b056254025770faa0fa789ba3b4e26b7bc7
Anastasia Demchenko <demchenkoanastasia1313@gmail.com>
2023-06-27 13:44:01 +0300

    header done / first-screen desktop

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,8 @@
-  
+$(document).ready(function() {
+    $('.first-screen-slider').slick({
+      dots: true,
+      Infinity: true,
+      appendArrows: '.first-screen-slider__arrows'
+    });
+
+  });
