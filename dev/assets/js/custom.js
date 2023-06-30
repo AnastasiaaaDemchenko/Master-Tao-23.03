15315a3ac651c7c1d5d6c5a95f622637b7d95afc
Anastasia Demchenko <demchenkoanastasia1313@gmail.com>
2023-06-30 12:47:40 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
     $('.first-screen-slider').slick({
       dots: true,
       Infinity: true,
-      appendArrows: '.first-screen-slider__arrows'
+      appendArrows: '.first-screen-slider__arrows',
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            arrows: false,
+          }
+        },
+      ]
     });
 
   });
